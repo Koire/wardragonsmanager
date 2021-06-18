7fe3451e81db8c87b7c7367e2d4c43f464c3ed3a
koire <pikewb@gmail.com>
2021-06-18 10:14:13 +0900

    removed firebase

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/database"
 
-firebase.initializeApp({
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-})
+// firebase.initializeApp({
+//     apiKey: process.env.apiKey,
+//     authDomain: process.env.authDomain,
+//     databaseURL: process.env.databaseURL,
+//     projectId: process.env.projectId,
+//     storageBucket: process.env.storageBucket,
+//     messagingSenderId: process.env.messagingSenderId,
+//     appId: process.env.appId,
+// })
 
 // const database = firebase.database().ref()
 // var postListRef = firebase.database().ref('posts');
